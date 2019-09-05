2fbaa2e8aac286d40e1526c869b2008a14e1f32a
JosueRS12 <53354777+JosueRS12@users.noreply.github.com>
2019-09-05 08:01:57 -0500

    Add files via upload

--- taller1.cpp
+++ taller1.cpp
@@ -10,6 +10,9 @@ struct Empleado{
 	int nh;
 	char ne[20];
 	char c[20];
+	int ed;
+	char j[20];
+	char cr[20];
 }empleado1;
 
 long int fib(int x){
@@ -58,6 +61,7 @@ void programa9(){
 		else if(empleado1.nh<240)
 			sb=(tf*empleado1.nh);
 		if(sb<tm)
+			sn=sb;
 			r=0;
 		if(sb>=tm&&sb<1000000){
 			r=sb*0.5;
@@ -81,6 +85,7 @@ void programa9(){
 		cout<<"retencion: "<<r<<endl;
 		i++;	
 		system("pause");
+		system("cls");
 	}	
 }
 void programa8(){
@@ -102,9 +107,102 @@ void programa8(){
 	system("pause");
 	system("cls");
 }
-
-
-
+void programa6(){	
+	cout<<"		Programa 6 \n";
+	i=10;
+	int k=1;
+	while(i!=0){
+		cout<<k++<<endl;
+		i--;
+	}
+	system("pause");
+	system("cls");
+}
+void programa14(){
+	int arr[10], k=0, n;
+	cout<<"		Programa 14 \n";
+	cout<<" Ingrese un numero en base 10 mayor a 0\n";
+	cin>>num;
+	n=num;
+	while(num>0){
+		arr[k]=num%2;
+		num/=2;
+		k++;
+	}
+	cout<<"El equivalente en binario es de "<<n<<" es: \n";
+	for(i=k-1;i>=0;i--){
+		cout<<arr[i];
+	}
+	system("pause");
+	system("cls");
+}
+void programa2(){
+	system("cls");
+	int n;
+	
+	string datos;
+	float seec=0,ceec=0,chin=0,peem=0,ceem=0,semin=0,pemin=0,ph=0,ch=0,phin=0,peec=0,cemin=0;
+	i=1;
+	cout<<"	Ingrese el numero de estudiantes \n";
+	cin>>n;
+	
+	while(i<=n){
+		cout<<"	Digite la edad:\n";
+		cin>>empleado1.ed;
+		cout<<" Ingrese el sexo:\n";
+		rewind(stdin);
+		getline(cin,datos);
+		strcpy(empleado1.s,datos.c_str());
+		
+		cout<<" Ingrese la carrera \n";
+		rewind(stdin);
+		getline(cin,datos);
+		strcpy(empleado1.cr,datos.c_str());
+	
+		cout<<" Ingrese la jornada \n";
+		rewind(stdin);
+		getline(cin,datos);
+		strcpy(empleado1.j,datos.c_str());
+		
+		if(strcmp(empleado1.cr,"contaduria")==0){
+			seec+=empleado1.ed;
+			ceec++; 
+		}
+		if(strcmp(empleado1.s,"masculino")==0&&strcmp(empleado1.cr,"ingenieria")==0&&strcmp(empleado1.j,"nocturna")==0)
+			chin++;
+		if(strcmp(empleado1.s,"femenino")==0&&empleado1.ed<20&&strcmp(empleado1.cr,"ingenieria")==0)
+			ceem++;
+		if(strcmp(empleado1.s,"femenino")==0&&strcmp(empleado1.cr,"ingenieria")==0)
+			semin+=empleado1.ed;
+			cemin++;
+		if(strcmp(empleado1.s,"masculino")==0&&strcmp(empleado1.cr,"derecho")==0&&strcmp(empleado1.j,"nocturna")==0&&empleado1.ed>22)
+			ch++;
+		peec=seec/ceec; //promedio de edad estudiantes contduria
+		phin=(chin/n)*100; //porcentaje hombres ingenieria noche
+		peem=(ceem/n)*100; //porcentaje mujeres menores a 20 años
+		pemin=semin/cemin; //promedio mujeres ingenieria
+		ph=(ch/n)*100; //porcentaje hombres derecho
+		i++;
+		system("cls");
+		
+	}
+		cout<<" promedio de edad estudiantes de contaduria es: "<<peec<<endl;
+		cout<<" porcentaje de estudiantes hombres que estudian ingenieria la noche: "<<phin<<endl;
+		cout<<" porcentaje de estudiantes mujeres que estudian ingenieria y son menores a 20 años "<<peem<<endl;
+		cout<<" promedio de estudiantes mujeres que estudian ingenieria "<<pemin<<endl;
+		cout<<" porcentaje de estudiantes hombres que estudian derecho y que son mayores a 22 años "<<ph<<endl;
+		system("pause");
+		system("cls");
+	
+}
+void programa5(){
+	cout<<"	Ingrese el numero \n";
+	do{
+		cin>>num;
+		if(num/)
+	}	
+	while(num!=0);
+}
 
 
 int main(){
@@ -112,30 +210,30 @@ int main(){
 	system("cls");
 	do{
 		cout<<"		Seleccione un programa \n";
-		cout<<"\n 1)";
-		cout<<" 2)";
-		cout<<" 3)";
-		cout<<" 4)";
-		cout<<" 5)";
-		cout<<" 6)";
-		cout<<" 7)";
-		cout<<" 8)";
-		cout<<" 9)\n";
-		cout<<" 10)";
-		cout<<" 11)";
-		cout<<" 12)";
-		cout<<" 13)";
-		cout<<" 14)";
-		cout<<" 15)";
+		cout<<"\n 1)\n";
+		cout<<" 2) Universidad\n";
+		cout<<" 3)\n";
+		cout<<" 4)\n";
+		cout<<" 5)\n";
+		cout<<" 6) Numeros del 1 al 10 \n";
+		cout<<" 7)\n";
+		cout<<" 8) Pertenece a serie fibonacci\n";
+		cout<<" 9) Salario de los empresarios\n";
+		cout<<" 10)\n";
+		cout<<" 11)\n";
+		cout<<" 12)\n";
+		cout<<" 13)\n";
+		cout<<" 14) Binario y hexadecimal\n";
+		cout<<" 15)\n";
 		cout<<" 16)\n";
-		cout<<" 17)";
-		cout<<" 18)";
-		cout<<" 19)";
-		cout<<" 20)";
-		cout<<" 22)";
-		cout<<" 23)";
+		cout<<" 17)\n";
+		cout<<" 18)\n";
+		cout<<" 19)\n";
+		cout<<" 20)\n";
+		cout<<" 22)\n";
+		cout<<" 23)\n";
 		cout<<" 24)\n";
-		cout<<" 25)";
+		cout<<" 25)\n";
 		cout<<" 00)Salir\n";
 		cin>>op;
 
@@ -144,14 +242,17 @@ int main(){
 			break;
 			case 8:	programa8();
 			break;
+			case 6:	programa6();
+			break;
+			case 14:programa14();
+			break;
+			case 2:programa2();
+			break;
 
 			default: cout<<"Salir \n";
 			break;
 		}
-	
-	
-	
-	}
+	} 
 	while(op!=00);
 	return 0;
 }
