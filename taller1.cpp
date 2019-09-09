01d1dcc3f772d85f6f00e38f345adb1afcc5c6be
JosueRS12 <53354777+JosueRS12@users.noreply.github.com>
2019-09-08 23:29:50 -0500

    Update taller1.cpp

--- taller1.cpp
+++ taller1.cpp
@@ -236,53 +236,526 @@ void programa7(){
 	system("cls");
 
 }
+void programa4(){
+	system("cls");
+	int n,i,a;
+	cout<<"		SUMA DE ENTEROS COMPRENDIDOS\n";
+	cout<<"Ingrese el numero \n";
+	cin>>n;
+	a=0;
+	if (n>1){
+		for(i=1;i<=n;i++){
+			cout<<"los numeros comprendidos son:\n"<<i<<endl;
+			a=a+i;
+		}
+		cout<<"la suma de todos los enteros es:\n"<<a<<endl;
+	}
+	
+	system ("pause");
+	system("cls");
+}
+void programa12(){
+	system("cls");
+	int cta,j=0,est[100],mat[100][100],l=0,k=0,sumnot[100][100];
+	double notas[100],promest=0,aux=0,promgrup=0,promgrupos[100],promediof=0,aux2=0;
+	cout<<"		PROMEDIO GRUPOS ESCOLARES\n";
+	cout<<"Ingrese la cantidad de grupos\n";
+	cin>>cta;
+	j=0;
+	for(i=0;i<cta;i++){
+		cout<<"Ingrese la cantidad de estudiantes del grupo "<<i+1<<endl;
+			cin>>est[j];
+			j++;
+	}
+	system("cls");
+	for(i=0;i<cta;i++){
+		cout<<" Para grupo "<<i+1<<endl;
+		for(j=0;j<est[i];j++){
+			cout<<"Ingrese la cantidad de materias que ve el alumno "<<j+1<<endl;
+			cin>>mat[i][j];
+			k=0;
+			aux2=0;
+			while(k!=mat[i][j]){
+				aux=0;
+				cout<<" Ingrese las notas de la materia "<< k+1<<endl;
+				for(l=0;l<3;l++){
+					cin>>notas[l];
+				}
+				aux=(notas[0]+notas[1]+notas[2])/3;
+				aux2+=aux;
+				k++;
+			}
+			sumnot[i][j]=aux2; //i es el grupo y j es el alumno
+			system("cls");
+		}
+		system("cls");
+	}	
+	//promedio de notas de cada alumno
+	for(i=0;i<cta;i++){
+		aux=0;
+		cout<<"\nGrupo "<<i+1<<endl;
+		for(j=0;j<est[i];j++){
+			cout<<" Promedio notas de estudiante "<<j+1<<endl;
+			promest=sumnot[i][j]/mat[i][j];
+		
+			aux+=promest;
+			cout<<" es: "<<promest<<endl;
+		}
+		cout<<"Promedio de notas grupo ";
+		promgrup=aux/est[i];
+		cout<<" es: "<<promgrup<<endl;
+		promgrupos[i]=promgrup;
+	}
+	aux=0;
+	for(i=0;i<cta;i++){
+		promediof+=promgrupos[i];
+	}
+	aux=promediof;
+	promediof=aux/cta;
+	cout<<"\n Promedio de los "<<cta<<" grupos es: "<<promediof<<endl;
+	
+	system("pause");
+	system("cls");
+}
+void programa18(){
+	int n,i;
+	cout<<"		SUMA, PRODUCTO Y DIFERENCIA VECTORES\n";
+	cout<< "Ingrese el tamaño del Arreglo A y Arreglo B\n";
+	cin>>n;
+	
+	int arregloA[n];
+	int arregloB[n];
+	
+	for(i=0;i<n;i++){
+		int num1;
+		cout<<"\n Ingrese los numeros que van en el arreglo A\n";
+		cin>>num1;
+		arregloA[i]=num1;
+	}
+	for(i=0;i<n;i++){
+		int num1;
+		num1=arregloA[i];
+		cout<<num1<<endl;
+	}
+		for(i=0;i<n;i++){
+		int num2;
+		cout<<"\n Ingrese los numeros que van en el arreglo B\n";
+		cin>>num2;
+		arregloB[i]=num2;
+	}
+		for(i=0;i<n;i++){
+		int num2;
+		num2=arregloB[i];
+		cout<<num2<<endl;
+	}
+	cout<<"\n SUMA DE LOS ARREGLOS\n";
+	int arregloC[n];
+	for(i=0;i<n;i++){
+		arregloC[i]=arregloA[i]+arregloB[i];
+		cout<<" "<<arregloC[i]<<endl;
+	
+	}
+	cout<<"\n RESTA DE LOS ARREGLOS\n";
+	for(i=0;i<n;i++){
+		arregloC[i]=arregloA[i]-arregloB[i];
+		cout<<" "<<arregloC[i]<<endl;
+	
+	}
+	cout<<"\n PRODUCTO DE LOS ARREGLOS\n";
+	for(i=0;i<n;i++){
+		arregloC[i]=arregloA[i]*arregloB[i];
+		cout<<" "<<arregloC[i]<<endl;
+	
+	}
+	
+	
+	
+	
+}
+void programa21(){
+	int n,i,contador=0,primoM=0,primoP=0;
+	cout<<"		Programa 21\n";
+	cout<< "Ingrese el tamaño del Arreglo\n";
+	cin>>n;
+	int Arr[n];
+	for(i=0;i<n;i++){
+		int num1;
+		cout<<"\n Ingrese los numeros que van en el arreglo \n";
+		cin>>num1;
+		Arr[i]=num1;
+	}
+	for(i=0;i<n;i++){
+		int num1;
+		num1=Arr[i];
+		cout<<endl<<num1<<endl;
+		for(int j=1;j<=num1;j++){
+			if(num1%j==0){
+				contador++;
+			}
+		}
+		if (contador==2){
+			cout<<" \n El numero es primo\n";
+			if(num1>primoM){
+				primoM=num1;
+				primoP=i;
+			}
+	
+		}else{
+			cout<<"\n El numero no es primo\n";
+			
+		}
+		contador=0;
+		
+	}
+	cout<<"\nEl mayor primo en el arreglo es:\n"<<primoM<<"\n y su posicion es:\n "<<primoP<<endl;
+}
+void programa3(){
+	string name;
+	system("cls");
+	int seg;
+	int paga,pagamayor,op,numhectareas;
+		cout<<"		FUMIGACION DE COSECHAS\n";
+		do{
+		cout<<"Ingrese el nombre"<<endl;
+		cin>>name;
+		cout<<" Seleccione el tipo de fumigacion que desea:"<<endl;
+		cout<<"1) contra malas hierbas, 10 dolares por hectarea\n";
+		cout<<"2) contra langostas, 15 dolares por hectarea\n";
+		cout<<"3) contra gusanos, 20 dolares por hectarea\n";
+		cout<<"4) contra todo lo anterior, 30 dolares por hectarea\n";
+		cin>>op;
+		system("cls");
+		cout<<"ingrese las hectareas a fumigar"<<endl;
+		cin>>numhectareas;
+		switch(op){
+			case 1: paga=numhectareas*10;
+			break;
+			case 2: paga=numhectareas*15;
+			break;
+			case 3: paga=numhectareas*20;
+			break;
+			case 4: paga=numhectareas*30;
+			break;
+			default: cout<<"Seleccione una opcion correcta\n";
+			break;
+		}
+		if (numhectareas>1000){
+			paga=paga-(paga*0.5);
+		}
+		if (paga>3000){
+			pagamayor=paga-3000;
+			pagamayor=pagamayor-(pagamayor*0.1);
+		}
+		system("cls");
+		cout<<name<<"debe pagar: "<<endl;
+		cout<<pagamayor+3000<<endl<<endl;
+		cout<<"Digite 1 para ingresar mas granjeros"<<endl;
+		cout<<"Digite 2 para salir al menu principal"<<endl;
+		cin>>seg;
+		system("cls");
+	}
+	while(seg==1);
+	system("cls");
+}
+void programa10(){
+	system("cls");
+	int n,sons,horasmes;
+	string nom,cod;
+	int sal,reten,salmensual,subsi;
+	cout<<"		SALARIO EMPLEADOS CON HIJOS\n";
+	cout<<"ingrese el numero de empleados"<<endl;
+	cin>>n;
+	for (int i=1;i<=n;i++){
+		cout<<"ingrese el nombre del empleado "<<i<<endl;
+		cin>>nom;
+		cout<<"ingrese el codigo de "<<nom<<endl;
+		cin>>cod;
+		cout<<"numero de hijos de"<<nom<<endl;
+		cin>>sons;
+		cout<<"ingrese el salario por hora de "<<nom<<endl;
+		cin>>sal;
+		cout<<"ingrese el numero de horas que trabaja por mes "<<nom<<endl;
+		cin>>horasmes;
+		salmensual=sal*horasmes;
+	    if(salmensual<428000){
+	    	if (sons<=12 && sons>6){
+	    		reten=((salmensual/100)*((12-sons)/2));
+				}
+		    if (sons<=6){
+		    	reten=((salmensual/100)*4);
+			}
+		}
+		if (salmensual>=428000 && salmensual<1000000){
+			if (sons<5){
+				reten=((salmensual/100)*5);
+			}
+			else if(sons>=5 && sons<10){
+				reten=10/sons;
+			} 
+			else
+				reten=0;
+		}
+ 		subsi=sons*17200;
+		 cout<<"La informacion del empleado "<<nom<<" de codigo: "<<cod<<endl;
+		 cout<<"devengado: "<<salmensual<<endl;
+		 cout<<"retencion: "<<reten<<endl;
+		 cout<<"subsidio: "<<subsi<<endl;
+		 cout<<"Total a pagar: "<<salmensual-reten+subsi<<endl;
+	}
+	system("pause");
+	system("cls");
+	
+}
+void programa11(){
+	system("cls");
+	int vendedor, articulo, i, j;
+	float venta, comision = 0;
+	cout<<"		MARANON\n";
+	cout<<"Ingrese el numero de vendedores"<<endl;
+	cin>>vendedor;
+	cout<<endl;
+	
+	for(i = 1; i <= vendedor; i++)
+	{
+		cout<<"Cuantos articulos vendio el "<<i<<" vendedor"<<endl;
+		cin>>articulo;
+		cout<<endl;
+		
+		for(j = 1; j <= articulo; j++)
+		{
+			cout<<"Ingrese el valor del "<<j<<" articulo vendido"<<endl;
+			cin>>venta;
+			cout<<endl;
+			
+			if(venta <= 100000)
+			{
+				comision = comision + ((venta * 10)/100);
+			}
+			else
+			{
+				comision = comision + ((venta * 7)/100);
+			}
+		}
+		system("cls");
+	}
+	
+	cout<<endl;
+	cout<<"El valor total de las comisiones es de: "<<comision<<endl;
+	system("pause");
+	system("cls");
+}
+void programa1(){
+	system("cls");
+	int op,retiro,sumret=0,contret=0,consig,sumconsig=0,contcon=0;
+	long int dif=0;
+	cout<<"		CAJERO\n";
+	do{
+			cout<<"\n Seleccione una opcion:\n";
+			cout<<"1) Retirar\n";
+			cout<<"2) Consignar\n";
+			cout<<"0) Salir\n";
+			cin>>op;
+			switch(op){
+				case 1:
+					cout<<"ingrese la suma de dinero a retirar\n";
+					cin>>retiro;
+					sumret+=retiro;
+					contret++;
+					system("cls");
+				break;
+				
+				case 2:
+					cout<<"ingrese la suma de dinero a consignar\n";
+					cin>>consig;
+					sumconsig+=consig;
+					contcon++;
+					system("cls");
+				break;
+				default: cout<<"Salir\n";
+				break;
+			}
+		}
+		while(op!=0);
+		system("cls");
+		cout<<"El total de dinero consigando fue de: "<<sumconsig<<" pesos\n";//dinero por consignaciones
+		cout<<"El total de dinero retirado fue de: "<<sumret<<" pesos\n";//dinero por retiro
+		if(contret>contcon){
+			cout<<"\nHubo mas retiros que consiganciones\n";
+			cout<<"El total de retiros fue: "<<contret<<endl;
+			cout<<"El total de consiganciones fue: "<<contcon<<endl;
+		}
+		else if(contcon>contret){
+			cout<<"\nHubo mas consignaciones que retiros\n";
+			cout<<"El total de consignaciones fue: "<<contcon<<endl;
+			cout<<"El total de retiros fue: "<<contret<<endl;
+		}
+		else {
+			cout<<"\nHubo igual  consignaciones y retiros\n";
+			cout<<"El total de consignaciones fue: "<<contcon<<endl;
+			cout<<"El total de retiros fue: "<<contret<<endl;
+		}
+		dif=sumconsig-sumret;
+		if(dif<0)
+			dif*=-1;
+		cout<<"\nLa diferencia absoluta fue de: "<<dif<<endl;
+		system("pause");
+		system("cls");
+}
+void programa22(){
+	system("cls");
+	int cta, vector[cta],fact=0,j=0,aux=0,sum=0,prom=0;
+	cout<<"		PROMEDIO FACTORIAL MATRIZ\n";
+	cout<<"Digite la cantidad de valores a ingresar \n";
+	cin>>cta;
+	cout<<"\n Digite un valor entero\n";
+	for(i=0;i<cta;i++){
+		cin>>vector[i];
+		j=1;
+		aux=1;
+		if(vector[i]==1){
+			fact=1;
+			sum+=fact;
+		}
+		else if(vector[i]!=1){
+			while(j<vector[i]){
+				aux=aux*(j+1);
+				j++;
+			}
+			fact=aux;
+			sum+=fact;
+		}
+	}
+	prom=sum/cta;    //promedio
+	cout<<" El promedio de los factoriales de los numeros ingresados es: "<<prom<<endl;
+	system("pause");
+	system("cls");
+}
+void programa23(){
+	system("cls");
+	int i,j,M,N,mayor,numMa=0;
+	cout<<"		REPETICIONES DEL NUMERO MAYOR MATRIZ\n";
+	cout<<"ingrese el numero filas matriz_a"<<endl;
+	cin>>M;
+	cout<<"ingrese el numero de columnas matriz_a"<<endl;
+	cin>>N;
+	int matriz_a[M][N];
+	i=0;
+	while (i<M)
+	{ j=0;
+	while(j<N)
+	{ cout<<"ingrese un numero entero en matriz_a["<<i<<"]["<<j<<"]"<<endl;
+	cin>>matriz_a[i][j];
+	j++;
+	}
+	i++;
+	}
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++)
+		{
+		cout<<matriz_a[i][j]<<" ";
+		}
+		cout<<endl;
+	
+	}	
+	mayor= matriz_a [0][0];
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++)
+		{
+		if (matriz_a[i][j]>mayor){
+			mayor=matriz_a [i][j];
+			}
+			
+		}
+	
+	}	
+		
+	for(i=0;i<M;i++){
+		for(j=0;j<N;j++)
+		{
+		if (matriz_a[i][j]==mayor){
+			numMa++;
+			}
+			
+		}
+	
+	}
+	cout<<"\n el numero mayor es: \n "<<mayor<<endl;
+	cout<<"\n se repite\n"<<numMa<<"\n veces\n"<<endl;							
+	cout<<"el tamaño de matriz_a es: "<<i<<","<<j<<endl;
+	system("pause");
+	system("cls");
+}
+
+
+
 
 int main(){
 	int op;
 	system("cls");
 	do{
 		cout<<"		Seleccione un programa \n";
-		cout<<"\n 1)\n";
+		cout<<"\n1) Cajero1)\n";
 		cout<<" 2) Universidad\n";
-		cout<<" 3)\n";
-		cout<<" 4)\n";
+		cout<<" 3) Fumigacion de cosechas\n";
+		cout<<" 4) Suma de enteros comprendidos\n";
 		cout<<" 5) Promedio digito 5\n";
 		cout<<" 6) Numeros del 1 al 10 \n";
 		cout<<" 7) Serie fibonacci\n";
 		cout<<" 8) Pertenece a serie fibonacci\n";
 		cout<<" 9) Salario de los empresarios\n";
-		cout<<" 10)\n";
-		cout<<" 11)\n";
-		cout<<" 12)\n";
+		cout<<" 10)Salario empleados con hijos\n";
+		cout<<" 11)Maranon\n";
+		cout<<" 12)Promedio grupos escolares\n";
 		cout<<" 13)\n";
 		cout<<" 14)Binario y hexadecimal\n";
 		cout<<" 15)\n";
 		cout<<" 16)\n";
 		cout<<" 17)\n";
-		cout<<" 18)\n";
+		cout<<" 18)Suma, producto y diferencia vectores\n";
 		cout<<" 19)\n";
 		cout<<" 20)\n";
-		cout<<" 22)\n";
-		cout<<" 23)\n";
+		cout<<" 21)vector mayor primo leido\n";
+		cout<<" 22)Promedio factorial matriz\n";
+		cout<<" 23)Repeticiones del numero mayor matriz\n";
 		cout<<" 24)\n";
 		cout<<" 25)\n";
 		cout<<" 00)Salir\n";
 		cin>>op;
 
 		switch(op) {
-			case 9: programa9();
+			case 9: programa9();//
 			break;
-			case 8:	programa8();
+			case 8:	programa8();//
 			break;
-			case 6:	programa6();
+			case 6:	programa6();//
 			break;
-			case 14:programa14();
+			case 14:programa14();//
 			break;
-			case 2:programa2();
+			case 2:programa2();//
 			break;
-			case 5:programa5();
+			case 5:programa5();//
 			break;
-			case 7:programa7();
+			case 7:programa7();//
+			break;
+			case 4:programa4();//
+			break;
+			case 12:programa12();//
+			break;
+			case 18:programa18();//
+			break;
+			case 21:programa21();//
+			break;
+			case 3:programa3();//
+			break;
+			case 10:programa10();//
+			break;
+			case 11:programa11();//
+			break;
+			case 1:programa1();//
+			break;
+			case 22:programa22();//
+			break;
+			case 23:programa23();//
 			break;
 			default: cout<<"Salir \n";
 			break;
